7238f3cb1a921819aaa55105e3c0a29b3e3ae072
mdshakil04 <m.shakil04@yahoo.com>
2024-05-10 19:07:58 +0600

    Payment route created

---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Payment = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center border-y-4 max-w-xs container py-4 mx-auto">
+        Payment
+      </h1>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
